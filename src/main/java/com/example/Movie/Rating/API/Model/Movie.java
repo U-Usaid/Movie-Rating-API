f1d45f87f5d899b735e4e3ac05d31d3ad07e175a
U-Usaid <osaid.osaid66@gmail.com>
2023-06-19 16:06:38 +0400

    adding table annotation

--- src/main/java/com/example/Movie/Rating/API/Model/Movie.java
+++ src/main/java/com/example/Movie/Rating/API/Model/Movie.java
@@ -4,23 +4,21 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
 @Data
 @Entity
+@Table(name = "movie")
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     Long id;
+
     String title;
     String genre;
-    Date release_year;
+    Date releaseYear;
 
 }
