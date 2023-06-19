61bb9f124211cd89734b8779f1190c39aa798281
U-Usaid <osaid.osaid66@gmail.com>
2023-06-19 11:57:21 +0400

    User Repository created

--- src/main/java/com/example/Movie/Rating/API/Repositories/UserRepository.java
+++ src/main/java/com/example/Movie/Rating/API/Repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.Movie.Rating.API.Repositories;
 
-public interface UserRepository {
+import com.example.Movie.Rating.API.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long > {
+
 }
