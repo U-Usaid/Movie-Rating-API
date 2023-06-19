eee2707f9d805cd369c6fb06fab11dbfe4643ef6
U-Usaid <osaid.osaid66@gmail.com>
2023-06-19 11:55:45 +0400

    User Class element placed

--- src/main/java/com/example/Movie/Rating/API/Model/User.java
+++ src/main/java/com/example/Movie/Rating/API/Model/User.java
@@ -1,4 +1,25 @@
 package com.example.Movie.Rating.API.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    Long id;
+    String username;
+    String password;
+
+
 }
