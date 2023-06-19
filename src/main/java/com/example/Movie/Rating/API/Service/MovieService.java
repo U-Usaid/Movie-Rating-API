36850625844047aff74c3664be473e4ad277303d
U-Usaid <osaid.osaid66@gmail.com>
2023-06-19 16:05:58 +0400

    few name changes

--- src/main/java/com/example/Movie/Rating/API/Service/MovieService.java
+++ src/main/java/com/example/Movie/Rating/API/Service/MovieService.java
@@ -12,23 +12,37 @@ public class MovieService {
     @Autowired
     MovieRepository movieRepository;
 
+
+
     //Movie Retrieval
-    public List<Movie> getMovies(){
-        return movieRepository.findAll();
+    public Movie getMovieById(Long id){
+        return movieRepository.findById(id).get();
     }
+
+
 
     //Movie Creation
     public Movie addMovies(Movie movie){
         return movieRepository.save(movie);
     }
 
+
+
     //Movie Update
-    public Movie editMovies(Movie movie){
-        return movieRepository.save(movie);
+    public Movie updateMovie(Long id, Movie movie) {
+        Movie existingMovie = movieRepository.findById(id).orElse(null);
+        if (existingMovie != null) {
+            existingMovie.setTitle(movie.getTitle());
+            existingMovie.setGenre(movie.getGenre());
+            existingMovie.setReleaseYear(movie.getReleaseYear());
+            return movieRepository.save(existingMovie);
+        }
+        return null;
     }
 
+
     //Movie Deletion
-    public void deletedMovies(Long id){
+    public void deleteMovie(Long id){
         movieRepository.deleteById(id);
     }
 
