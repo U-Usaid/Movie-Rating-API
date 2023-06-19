5dabf37795eec6ce3aa96720c128615eb085210f
U-Usaid <osaid.osaid66@gmail.com>
2023-06-19 11:56:45 +0400

    Movie Repository created

--- src/main/java/com/example/Movie/Rating/API/Repositories/MovieRepository.java
+++ src/main/java/com/example/Movie/Rating/API/Repositories/MovieRepository.java
@@ -1,4 +1,9 @@
 package com.example.Movie.Rating.API.Repositories;
 
-public interface MovieRepository {
+import com.example.Movie.Rating.API.Model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository  extends JpaRepository<Movie,Integer> {
 }
