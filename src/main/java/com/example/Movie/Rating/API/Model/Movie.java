d95dcf9d58563bcf878589b035e17a2eb4bf0863
U-Usaid <osaid.osaid66@gmail.com>
2023-06-19 11:56:06 +0400

    Movie Class element placed

--- src/main/java/com/example/Movie/Rating/API/Model/Movie.java
+++ src/main/java/com/example/Movie/Rating/API/Model/Movie.java
@@ -1,4 +1,26 @@
 package com.example.Movie.Rating.API.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@Data
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    Integer id;
+    String title;
+    String genre;
+    Date release_year;
+
 }
