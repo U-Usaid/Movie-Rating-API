bee8af93b8f139740a602c4e19733fe8b34d8549
U-Usaid <osaid.osaid66@gmail.com>
2023-06-19 16:05:14 +0400

    adding Table Annotation for users

--- src/main/java/com/example/Movie/Rating/API/Model/User.java
+++ src/main/java/com/example/Movie/Rating/API/Model/User.java
@@ -4,20 +4,18 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Data
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     Long id;
+
     String username;
     String password;
 
