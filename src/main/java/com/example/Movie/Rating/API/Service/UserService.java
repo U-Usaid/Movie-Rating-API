ff0fbdda2dfb323eaf374face2a72a370b12920f
U-Usaid <osaid.osaid66@gmail.com>
2023-06-20 13:58:28 +0400

    add user service

--- src/main/java/com/example/Movie/Rating/API/Service/UserService.java
+++ src/main/java/com/example/Movie/Rating/API/Service/UserService.java
@@ -2,24 +2,36 @@ package com.example.Movie.Rating.API.Service;
 
 import com.example.Movie.Rating.API.Model.User;
 import com.example.Movie.Rating.API.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
+//    private final
+    @Autowired
+    UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+//    public UserService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+//    public User registerUser(User user) {
+//        // Check if the username is already taken
+//        if (userRepository.findByUsername(user.getUsername()) != null) {
+//            throw new IllegalArgumentException("Username already exists");
+//        }
+//
+//        // Save the user to the database
+//        return userRepository.save(user);
+//    }
+
+    public User add(User user){
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(user.getPassword());
+        return userRepository.save(newUser);
     }
 
-    public User registerUser(User user) {
-        // Check if the username is already taken
-        if (userRepository.findByUsername(user.getUsername()) != null) {
-            throw new IllegalArgumentException("Username already exists");
-        }
 
-        // Save the user to the database
-        return userRepository.save(user);
-    }
 }
 
